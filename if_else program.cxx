8849bc61e444b0232a9f9fd01537844f89013dcd
arishkhan54 <114579033+arishkhan54@users.noreply.github.com>
2022-10-04 01:01:00 +0530

    Add files via upload

---
+++ if_else program.cxx
@@ -0,0 +1,31 @@
+
+
+
+#include<stdio.h>
+#include<conio.h>
+ void main()
+  {
+  clrscr();
+  int number=0;
+   printf("Enter a number:\n");
+   scanf("%d",&number);
+
+   if(number==10)
+   {
+   printf("number is equal 10");
+   }
+   else if(number==50)
+   {
+
+   printf("number is equal 50");
+   }
+   else if(number==100)
+   {
+   printf("number is equal to 100");
+   }
+   else
+   {
+   printf("number is not equal to 10,50,100");
+   }
+   getch();
+   }
